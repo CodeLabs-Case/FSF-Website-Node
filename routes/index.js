916a584ae979f226c1babd14cc3214898dd9f231
blackBoxMod <davis.architect99@gmail.com>
2023-11-01 11:17:35 -0400

    Added route logic to handle localhost

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ router.use("/", require('./home.js'))
 router.use("/agreements", require('./agreements.js'))
 router.use("/services", require('./services.js'))
 router.use("/galleries", require('./galleries.js'))
+router.use("/admin", require('./admin.js'))
 
 
 
